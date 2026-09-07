a71cf569e1f46d0903de8c35a603174f7e1ea3dd
Subhojit <subhojitp7@gmail.com>
2026-09-07 23:09:22 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c: s){
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else if(st.empty() || (c==')' && st.top()!='(') ||
+                (c=='}' && st.top()!='{') || (c==']' && st.top()!='[')){
+                // cout << c << " ";
+                return false;
+            }
+            else
+                st.pop();
+        }
+        return st.empty();
+    }
+};
